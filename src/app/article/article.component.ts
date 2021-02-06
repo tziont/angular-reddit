5b423ec971867654e333ef443d748e9bf6f1736b
tzione <tzion@worldwideprep.com>
2021-02-06 19:28:54 +0200

    wow

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,32 @@
+import { Article } from '../article.model';
+import { Component, HostBinding, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  article: Article;
+
+  constructor() {
+    this.article = new Article(
+      'Angular',
+      'http://angular.io',
+      10);
+  }
+
+  voteUp() {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown() {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit(): void {
+  }
+}
